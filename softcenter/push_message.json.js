eaaa2077b0bd3be0197828e3e7be200d23787701
Xu Zhaowei <paldier@hotmail.com>
2020-03-13 17:02:42 +0800

    update push_message

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,10 +1,10 @@
 {
 "title":"<em>梅林固件&nbsp;-&nbsp;软件中心</em>",
-"content1":"<b>软件中心：新版1.2.5已经更新，添加多語言支持</b>,务必先更新固件到5.0.8或以上<font color='#1E90FF'>在更新软件中心",
-"content2":"固件最新版本：5.0.9:优化插件启动稳定性",
-"content3":"部分插件下载缓慢，5分钟后刷新查看",
-"content4":"<font color='#1E90FF'>交流反馈:&nbsp;&nbsp;</font>\
+"content1":"<b>软件中心：</b>务必先更新固件到5.0.8或以上<font color='#1E90FF'>在更新软件中心",
+"content2":"部分插件下载缓慢，5分钟后刷新查看",
+"content3":"<font color='#1E90FF'>交流反馈:&nbsp;&nbsp;</font>\
 <a href='https://github.com/paldier/softcenterarm64' target='_blank'><em>1.软件中心GitHub项目</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='https://t.me/merlinchat' target='_blank'><em>2.加入telegram群</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
-<a href='https://www.right.com.cn/forum/forum-173-1.html' target='_blank'><em>3.恩山论坛插件版块</em></a>"
+<a href='https://www.right.com.cn/forum/forum-173-1.html' target='_blank'><em>3.恩山论坛插件版块</em></a>",
+"content4":"<font color='#1E90FF'>当前固件版本:</font><b>"+db_softcenter_["softcenter_firmware_version"]+"</b>&nbsp;&nbsp;<font color='#1E90FF'>最新固件版本:</font><b>5.0.9</b>"
 }
--- softcenter/push_message_en.json.js
+++ softcenter/push_message_en.json.js
@@ -1,11 +1,11 @@
 {
 "title":"<em>MerlinR&nbsp;-&nbsp;Software Center</em>",
-"content1":"<b>Software Center: Latset version:1.2.5</b>,First update the firmware to 5.0.8 or later,<font color='#1E90FF'> then update the software center</font>",
-"content2":"Latset firmware version：5.0.9",
-"content3":"some plugins download slowly,Refresh the page after 5 minutes",
-"content4":"<font color='#1E90FF'>feedback:&nbsp;&nbsp;</font>\
+"content1":"<b>Software Center:</b>First update the firmware to 5.0.8 or later,<font color='#1E90FF'> then update the software center</font>",
+"content2":"some plugins download slowly,Refresh the page after 5 minutes",
+"content3":"<font color='#1E90FF'>feedback:&nbsp;&nbsp;</font>\
 <a href='https://github.com/paldier/softcenterarm64' target='_blank'><em>1.GitHub</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='https://t.me/merlinchat' target='_blank'><em>2.join telegram</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
-<a href='https://www.right.com.cn/forum/forum-173-1.html' target='_blank'><em>3.chinese forum</em></a>"
+<a href='https://www.right.com.cn/forum/forum-173-1.html' target='_blank'><em>3.chinese forum</em></a>",
+"content4":"<font color='#1E90FF'>Firmware version:</font><b>"+db_softcenter_["softcenter_firmware_version"]+"</b>&nbsp;&nbsp;<font color='#1E90FF'>Latset version:</font><b>5.0.9</b>"
 }
 

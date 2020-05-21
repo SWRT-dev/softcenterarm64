c9b21307e74435587d806e3e666579c5a62734cf
Xu Zhaowei <paldier@hotmail.com>
2020-05-21 11:00:30 +0800

    repack softether

--- softether/config.json.js
+++ softether/config.json.js
@@ -1,8 +1,8 @@
 {
 "version":"1.9",
-"md5":"1f04ab72bb98364fcda216c5fd3aa1ec",
+"md5":"4b9d06a4d79dbdded7085544a156b975",
 "home_url":"Module_softether.asp",
 "title":"SoftEther_VPN_Server",
 "description":"VPN全家桶, ver 4.29 build 9680",
-"build_date":"2019-10-04_22:29:18"
+"build_date":"2020-05-21_11:00:15"
 }
--- softether/softether/install.sh
+++ softether/softether/install.sh
@@ -1,7 +1,14 @@
 #! /bin/sh
 source /jffs/softcenter/scripts/base.sh
 alias echo_date='echo 【$(TZ=UTC-8 date -R +%Y年%m月%d日\ %X)】:'
+MODEL=$(nvram get productid)
 DIR=$(cd $(dirname $0); pwd)
+touch /tmp/kp_log.txt
+if [ "$MODEL" == "GT-AC5300" ] || [ "$MODEL" == "GT-AX11000" ] || [ "$MODEL" == "GT-AC2900" ] || [ "$(nvram get merlinr_rog)" == "1" ];then
+	ROG=1
+elif [ "$MODEL" == "TUF-AX3000" ] || [ "$(nvram get merlinr_tuf)" == "1" ] ;then
+	TUF=1
+fi
 
 # stop softether first
 enable=`dbus get softether_enable`
@@ -16,21 +23,16 @@ cp -rf /tmp/softether/bin/* /jffs/softcenter/bin/
 cp -rf /tmp/softether/scripts/* /jffs/softcenter/scripts/
 cp -rf /tmp/softether/webs/* /jffs/softcenter/webs/
 cp -rf /tmp/softether/res/* /jffs/softcenter/res/
-rm -rf /tmp/softether* >/dev/null 2>&1
+
 chmod 755 /jffs/softcenter/bin/*
 chmod 755 /jffs/softcenter/scripts/*
-if [ "`nvram get productid`" == "BLUECAVE" ];then
-	cp -r /jffs/softcenter/scripts/softether.sh /jffs/softcenter/init.d/S98SoftEther.sh
-	cp -r /jffs/softcenter/scripts/softether.sh /jffs/softcenter/init.d/N98SoftEther.sh
-else
-	ln -sf /jffs/softcenter/scripts/softether.sh /jffs/softcenter/init.d/S98SoftEther.sh
-	ln -sf /jffs/softcenter/scripts/softether.sh /jffs/softcenter/init.d/N98SoftEther.sh
-fi
+ln -sf /jffs/softcenter/scripts/softether.sh /jffs/softcenter/init.d/S98SoftEther.sh
+ln -sf /jffs/softcenter/scripts/softether.sh /jffs/softcenter/init.d/N98SoftEther.sh
 
 # 离线安装用
 dbus set softether_version="$(cat $DIR/version)"
 dbus set softcenter_module_softether_version="$(cat $DIR/version)"
-dbus set softcenter_module_softether_description="VPN全家桶, ver 4.29 build 9680"
+dbus set softcenter_module_softether_description="VPN全家桶, ver 4.30 build 9700"
 dbus set softcenter_module_softether_install="1"
 dbus set softcenter_module_softether_name="softether"
 dbus set softcenter_module_softether_title="SoftEther_VPN_Server"

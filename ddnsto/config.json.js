d808573a535c07ca7e400b0b84466f9a0cd21a76
Xu Zhaowei <paldier@hotmail.com>
2021-01-24 11:28:05 +0800

    add ddnsto

---
+++ ddnsto/config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"2.9.3",
+"md5":"141e5767e58b613e2a8095fe45b23302",
+"home_url":"Module_ddnsto.asp",
+"title":"DDNSTO远程控制",
+"description":"DDNSTO远程控制",
+"build_date":"2021-01-24_11:26:41"
+}

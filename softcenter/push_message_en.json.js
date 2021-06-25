53c64c43da3bdaa84f5e0b66e6bcc080d5e2f924
Xu Zhaowei <paldier@hotmail.com>
2021-06-25 08:35:51 +0800

    update

--- softcenter/push_message_en.json.js
+++ softcenter/push_message_en.json.js
@@ -1,11 +1,11 @@
 {
 "title":"<em>Software Center&nbsp;for&nbsp;arm64&nbsp;platform</em>",
-"content1":"<b>Software Center: </b>1.3.3,<font color='#1E90FF'> Support api version 1.1 and 1.5</font>",
-"content2":"New firmware 5.1.4 has been released",
+"content1":"<b>Software Center: </b>1.3.5,<font color='#1E90FF'> Support api version 1.1 and 1.5</font>",
+"content2":"New firmware 5.1.5 has been released",
 "content3":"<font color='#1E90FF'>feedback:&nbsp;&nbsp;</font>\
 <a href='https://github.com/paldier/softcenterarm64' target='_blank'><em>1.GitHub</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='https://t.me/merlinchat' target='_blank'><em>2.join telegram</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='https://www.right.com.cn/forum/forum-173-1.html' target='_blank'><em>3.chinese forum</em></a>",
-"content4":"<font color='#1E90FF'>Firmware version:</font><b>"+db_softcenter_["softcenter_firmware_version"]+"</b>&nbsp;&nbsp;<font color='#1E90FF'>Latset version:</font><b>5.1.4</b>&nbsp;&nbsp;<a href='https://github.com/MerlinRdev/Supported_Devices/blob/master/Firmware_Download.md' target='_blank'><em>Download</em></a>"
+"content4":"<font color='#1E90FF'>Firmware version:</font><b>"+db_softcenter_["softcenter_firmware_version"]+"</b>&nbsp;&nbsp;<font color='#1E90FF'>Latset version:</font><b>5.1.5</b>&nbsp;&nbsp;<a href='https://github.com/MerlinRdev/Supported_Devices/blob/master/Firmware_Download.md' target='_blank'><em>Download</em></a>"
 }
 
